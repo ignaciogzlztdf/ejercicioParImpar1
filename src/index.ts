37ef382bfd49193706a26f590b90b72c6e35f6cc
ignaciogzlztdf <104226794+ignaciogzlztdf@users.noreply.github.com>
2022-05-05 10:58:28 -0300

    Pasé este ejercicio que estaba en el repositorio de tareas y lo dejé acá para que sea más fácil de visualizar.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("que capos que somos");
+import "./styles.css";
+
+let numeroIngresado: number | null = Number(
+  prompt("Ingrese un número para determinar si es par o impar.")
+);
+
+if (numeroIngresado === 0) {
+  alert("El número ingresado es 0.");
+} else if (numeroIngresado % 2 === 0) {
+  alert("El número ingresado es par.");
+} else {
+  alert("El número ingresado es impar.");
+}
